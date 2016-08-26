e91ea2b9b2a0e8d889fada49dc4a050e6d354962
whdcumt <wanghongdong0@126.com>
2016-08-26 11:03:17 +0800

    第一种产生段错误的原因：给空地址赋值，以及段错误的调试过程

---
+++ bugging.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void segfault()
+{
+  int *p=NULL;
+  *p=100;
+}
+int main(void)
+{
+    segfault();
+ /*   char buf[1]="a";
+      buf[10003]='A'
+     printf("%c\n",buf[10003]);
+  */
+     return 0;
+}

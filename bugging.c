38d792cae496a99ef1c38a457d6cdb6becdf1039
whdcumt <wanghongdong0@126.com>
2016-08-26 11:46:51 +0800

    数组越界段错误调试结束

--- bugging.c
+++ bugging.c
@@ -9,7 +9,7 @@ int main(void)
 {
  //    segfault();
       char buf[2]="a";
-      buf[10003]='A';
-     printf("%c\n",buf[10003]);
+      buf[10]='A';
+      printf("%c\n",buf[10]);
      return 0;
 }

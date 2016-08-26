af1d5236e67789ad2781b652753ca4752767adf6
whdcumt <wanghongdong0@126.com>
2016-08-26 11:34:09 +0800

    数组越界错误（1）

--- bugging.c
+++ bugging.c
@@ -7,10 +7,9 @@ void segfault()
 }
 int main(void)
 {
-    segfault();
- /*   char buf[1]="a";
-      buf[10003]='A'
+ //    segfault();
+      char buf[2]="a";
+      buf[10003]='A';
      printf("%c\n",buf[10003]);
-  */
      return 0;
 }

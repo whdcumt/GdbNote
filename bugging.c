4336d913a7e8518fc3354b980fa37665d856b99b
whdcumt <wanghongdong0@126.com>
2016-08-26 12:33:50 +0800

    GDB利用core文件调试错误

--- bugging.c
+++ bugging.c
@@ -7,9 +7,10 @@ void segfault()
 }
 int main(void)
 {
- //    segfault();
-      char buf[2]="a";
-      buf[10]='A';
-      printf("%c\n",buf[10]);
+    segfault();
+ /*   char buf[1]="a";
+      buf[10003]='A'
+     printf("%c\n",buf[10003]);
+  */
      return 0;
 }
